a66ad31b70283fb2ee00e7a33b913e86e6ec9237
Gökhan Arkan <gokhanarkan@gmail.com>
2021-04-15 12:09:53 +0300

    implemented app ui

--- next-app/components/Layout.js
+++ next-app/components/Layout.js
@@ -1,17 +1,253 @@
 import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
 
-import Navigation from "./Navigation";
+import { useRouter } from "next/router";
+
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import {
+  CollectionIcon,
+  HomeIcon,
+  MenuAlt2Icon,
+  PhotographIcon,
+  XIcon,
+} from "@heroicons/react/outline";
+import { SearchIcon } from "@heroicons/react/solid";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const sidebarNavigation = [
+    {
+      name: "Home",
+      href: "/",
+      icon: HomeIcon,
+      current: router.pathname === "/",
+    },
+    {
+      name: "Add Location",
+      href: "/add",
+      icon: PhotographIcon,
+      current: router.pathname === "/add",
+    },
+    {
+      name: "Map View",
+      href: "/map",
+      icon: CollectionIcon,
+      current: router.pathname === "/map",
+    },
+  ];
+
   return (
-    <div>
+    <>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Navigation />
-      <div pt={4}>{children}</div>
-    </div>
+      <div className="h-screen bg-gray-50 flex overflow-hidden">
+        {/* Narrow sidebar */}
+        <div className="hidden w-28 bg-blue-700 overflow-y-auto md:block">
+          <div className="w-full py-6 flex flex-col items-center">
+            <div className="flex-shrink-0 flex items-center">
+              <Image
+                src="/logo192.png"
+                alt="Logo"
+                className="w-auto"
+                height={64}
+                width={64}
+              />
+            </div>
+            <div className="flex-1 mt-6 w-full px-2 space-y-1">
+              {sidebarNavigation.map((item) => (
+                <Link href={item.href}>
+                  <a
+                    key={item.name}
+                    className={classNames(
+                      item.current
+                        ? "bg-blue-800 text-white"
+                        : "text-blue-100 hover:bg-blue-800 hover:text-white",
+                      "group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium text-center"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    <item.icon
+                      className={classNames(
+                        item.current
+                          ? "text-white"
+                          : "text-blue-300 group-hover:text-white",
+                        "h-6 w-6"
+                      )}
+                      aria-hidden="true"
+                    />
+                    <span className="mt-2">{item.name}</span>
+                  </a>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* Mobile menu */}
+        <Transition.Root show={mobileMenuOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            static
+            className="md:hidden"
+            open={mobileMenuOpen}
+            onClose={setMobileMenuOpen}
+          >
+            <div className="fixed inset-0 z-40 flex">
+              <Transition.Child
+                as={Fragment}
+                enter="transition-opacity ease-linear duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity ease-linear duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+              </Transition.Child>
+              <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                <div className="relative max-w-xs w-full bg-blue-700 pt-5 pb-4 flex-1 flex flex-col">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="absolute top-1 right-0 -mr-14 p-1">
+                      <button
+                        type="button"
+                        className="h-12 w-12 rounded-full flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-white"
+                        onClick={() => setMobileMenuOpen(false)}
+                      >
+                        <XIcon
+                          className="h-6 w-6 text-white"
+                          aria-hidden="true"
+                        />
+                        <span className="sr-only">Close sidebar</span>
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  <div className="flex-shrink-0 px-4 flex items-center">
+                    <Image
+                      src="/logo192.png"
+                      alt="Logo"
+                      className="w-auto"
+                      height={64}
+                      width={64}
+                    />
+                  </div>
+                  <div className="mt-5 flex-1 h-0 px-2 overflow-y-auto">
+                    <nav className="h-full flex flex-col">
+                      <div className="space-y-1">
+                        {sidebarNavigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? "bg-blue-800 text-white"
+                                : "text-blue-100 hover:bg-blue-800 hover:text-white",
+                              "group py-2 px-3 rounded-md flex items-center text-sm font-medium"
+                            )}
+                            aria-current={item.current ? "page" : undefined}
+                          >
+                            <item.icon
+                              className={classNames(
+                                item.current
+                                  ? "text-white"
+                                  : "text-blue-300 group-hover:text-white",
+                                "mr-3 h-6 w-6"
+                              )}
+                              aria-hidden="true"
+                            />
+                            <span>{item.name}</span>
+                          </a>
+                        ))}
+                      </div>
+                    </nav>
+                  </div>
+                </div>
+              </Transition.Child>
+              <div className="flex-shrink-0 w-14" aria-hidden="true">
+                {/* Dummy element to force sidebar to shrink to fit close icon */}
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
+
+        {/* Content area */}
+        <div className="flex-1 flex flex-col overflow-hidden">
+          <header className="w-full">
+            <div className="relative z-10 flex-shrink-0 h-16 bg-white border-b border-gray-200 shadow-sm flex">
+              <button
+                type="button"
+                className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500 md:hidden"
+                onClick={() => setMobileMenuOpen(true)}
+              >
+                <span className="sr-only">Open sidebar</span>
+                <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
+              </button>
+              <div className="flex-1 flex justify-between px-4 sm:px-6">
+                <div className="flex-1 flex">
+                  <form className="w-full flex md:ml-0" action="#" method="GET">
+                    <label htmlFor="search_field" className="sr-only">
+                      Search all files
+                    </label>
+                    <div className="relative w-full text-gray-400 focus-within:text-gray-600">
+                      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
+                        <SearchIcon
+                          className="flex-shrink-0 h-5 w-5"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <input
+                        name="search_field"
+                        id="search_field"
+                        className="h-full w-full border-transparent py-2 pl-8 pr-3 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-0 focus:border-transparent focus:placeholder-gray-400"
+                        placeholder="Search"
+                        type="search"
+                      />
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </header>
+          <div className="flex-1 flex items-stretch overflow-hidden">
+            <main className="flex-1 overflow-y-auto">
+              <section
+                aria-labelledby="primary-heading"
+                className="min-w-0 flex-1 h-full flex flex-col overflow-hidden lg:order-last"
+              >
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                  <div className="max-w-3xl mx-auto">{children}</div>
+                </div>
+              </section>
+            </main>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
